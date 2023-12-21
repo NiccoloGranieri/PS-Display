4a51a0e9ff573450226be1371d81e5a1c18bf281
Niccolò <granieriniccolo@gmail.com>
2023-12-21 12:36:33 +0100

    Uninstall kivy

--- PS-DIsplay.py
+++ PS-DIsplay.py
@@ -1,19 +1,24 @@
-from psnawp_api import PSNAWP
+# from psnawp_api import PSNAWP
 
 psnawp = PSNAWP('')
 
-# This is you
-client = psnawp.me()
+# # This is you
+# client = psnawp.me()
 
-print("Online ID")
-print(client.online_id)
-print("Account ID")
-print(client.account_id)
+# print("Online ID")
+# print(client.online_id)
+# print("Account ID")
+# print(client.account_id)
 
-print("Baldur's Gate 3 Trophies")
-for trophy_title in client.trophy_titles_for_title(title_ids=['PPSA14002_00']):
-    print(trophy_title)
+# print("Baldur's Gate 3 Trophies")
+# for trophy_title in client.trophy_titles_for_title(title_ids=['PPSA14002_00']):
+#     print(trophy_title)
 
-print("Play Times")
-titles_with_stats = client.title_stats()
-print(list(titles_with_stats))
+# print("Play Times")
+# titles_with_stats = client.title_stats()
+# print(list(titles_with_stats))
+
+from guizero import App
+
+app = App(title="Hello world")
+app.display()
